b5d37633212da46b58789b62a704b97fcd59068f
Danila_notebook <lap6pyk@gmail.com>
2022-05-16 17:28:33 -0300

    4_16_05

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 //import { io } from "socket.io-client"; .use(io)
 
-import { Vue } from 'vue'
-Vue.prototype.$hostname = 'https://safe-fjord-51597.herokuapp.com' //http://localhost:5000
 
-createApp(App).use(store).use(router).mount('#app')
+const appVue = createApp(App).use(store).use(router).mount('#app')
+appVue.config.globalProperties.$hostname = 'https://safe-fjord-51597.herokuapp.com'
