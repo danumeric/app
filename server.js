f81968e10625ca85255604ea4ded39ece21b262a
danumeric <89909851+danumeric@users.noreply.github.com>
2022-07-13 21:46:14 +0300

    add logic aut8

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+import router from "@/router";
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use('/reg', serveStatic(path.join(__dirname, '/dist/reg')))
+app.use('/reg', router.push("/auth"))
 
 
 const port = process.env.PORT || 8080
