9731ff5a0b4f56c7775270b836154ab55be73c24
danumeric <89909851+danumeric@users.noreply.github.com>
2022-07-13 22:20:05 +0300

    add logic aut11

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+//const serveStatic = require('serve-static')
 const path = require('path')
+
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+const history = require('connect-history-api-fallback');//
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-//app.use('/auth', router.push("/auth"))
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});//app.use('/auth', router.push("/auth"))
 
 
 const port = process.env.PORT || 8080
 
-app.listen(port)
-console.log('front launch on port' + port);
+var server = app.listen(port || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
